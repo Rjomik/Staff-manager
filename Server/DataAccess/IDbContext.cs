5c15781cf566800acc81606a548a8d432198be40
Roman Rychkov <roman.rychkov@outlook.com>
2023-12-04 00:59:16 +0300

    Skeleton of the service implementation Database interface

---
+++ Server/DataAccess/IDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.Collections;
+using Grpc.Core;
+using GrpcApi;
+
+namespace Server
+{
+    public interface IDbContext
+    {
+        Task AddWorkers(IAsyncStreamReader<AddWorkersRequest> requestStream, IServerStreamWriter<CrudOperationResponse> responseStream);
+        Task<WorkerMessage> GetWorker(long id);
+        Task GetWorkersStream(IServerStreamWriter<WorkerMessage> responseStream);
+        Task RemoveWorkers(RepeatedField<long> id, IServerStreamWriter<CrudOperationResponse> responseStream);
+        Task UpdateWorkers(IAsyncStreamReader<UpdateWorkersRequest> requestStream, IServerStreamWriter<CrudOperationResponse> responseStream);
+    }
+}
--- Server/GrpcServiceImpl.cs
+++ Server/GrpcServiceImpl.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrpcApi;
+using Grpc.Core;
 
 namespace Server
 {
-    internal class GrpcServiceImpl
+    internal class GrpcServiceImpl : WorkerIntegration.WorkerIntegrationBase
     {
+        IDbContext dbContext;
+
+        public override async Task AddWorkers(IAsyncStreamReader<AddWorkersRequest> requestStream, IServerStreamWriter<CrudOperationResponse> responseStream, ServerCallContext context)
+        {
+            //todo validation of fields
+            await dbContext.AddWorkers(requestStream, responseStream);
+        }
+
+        public override async Task<WorkerMessage> GetWorker(GetWorkerRequest request, ServerCallContext context)
+        {
+            return await dbContext.GetWorker(request.Id);
+        }
+
+        public override async Task GetWorkersStream(GetWorkersStreamRequest request, IServerStreamWriter<WorkerMessage> responseStream, ServerCallContext context)
+        {
+            await dbContext.GetWorkersStream(responseStream);
+        }
+
+        public override async Task RemoveWorkers(RemoveWorkersRequest request, IServerStreamWriter<CrudOperationResponse> responseStream, ServerCallContext context)
+        {
+            await dbContext.RemoveWorkers(request.Id, responseStream);
+        }
+
+        public override async Task UpdateWorkers(IAsyncStreamReader<UpdateWorkersRequest> requestStream, IServerStreamWriter<CrudOperationResponse> responseStream, ServerCallContext context)
+        {
+            await dbContext.UpdateWorkers(requestStream, responseStream);
+        }
     }
 }
