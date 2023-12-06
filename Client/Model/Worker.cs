c91ee636dc9435646d73e8c2df8036faac9845ae
Roman Rychkov <roman.rychkov@outlook.com>
2023-12-06 14:30:02 +0300

    main xaml page structure

---
+++ Client/Model/Worker.cs
@@ -0,0 +1,52 @@
+﻿using GrpcApi;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class Worker
+    {
+        public Worker()
+        {
+
+        }
+
+        public Worker(WorkerMessage grpcSource)
+        {
+            Id = grpcSource.Id;
+            LastName = grpcSource.LastName;
+            FirstName = grpcSource.FirstName;
+            MiddleName = grpcSource.MiddleName;
+            Birthday = new DateTime( grpcSource.Birthday, DateTimeKind.Utc);
+            Sex = (Sex)(int)grpcSource.Sex;
+            HasChildren = grpcSource.HasChildren;
+        }
+
+        public virtual long Id { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string MiddleName { get; set; }
+        public virtual DateTime Birthday { get; set; }
+        public virtual Sex Sex { get; set; }
+        public virtual bool HasChildren { get; set; }
+
+        public virtual WorkerMessage ToWorkerMessage()
+        {
+            return new WorkerMessage()
+            {
+                Id = Id,
+                LastName = LastName,
+                FirstName = FirstName,
+                MiddleName = MiddleName,
+                Birthday = Birthday.Ticks,
+                Sex = (GrpcApi.Sex)(int)Sex,
+                HasChildren = HasChildren
+            };
+        }
+    }
+    public enum Sex { Male = 0, Female = 1 }
+}
---
+++ Client/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GrpcApi;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class MainWindowViewModel
+    {
+        ObservableCollection<Worker> Workers = new ObservableCollection<Worker>();
+
+        Worker selectedWorker;
+        public Worker SelectedWorker { get => selectedWorker; set => selectedWorker = value; }
+
+    }
+}
